3ae0f8204218fe154a6bcbf83613fa64b8a7ce92
jicius <bq_ji@yahoo.com>
2017-09-07 16:26:58 +0800

    fix: 修改test文件头信息

--- tests/test_raven_python.py
+++ tests/test_raven_python.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #   -*- coding: utf-8 -*-
 #
-#   Copyright (C) 2017 omega
+#   Copyright (C) 2017 Jicius
 # 
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
